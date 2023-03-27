141abee01ed7f4509250e0f14241e32585aa39f6
chat0933 <90390114+chat0933@users.noreply.github.com>
2023-03-27 14:46:34 +0200

    Test
    test1

--- iot_projekt/MakeDB.py
+++ iot_projekt/MakeDB.py
@@ -1,8 +1,8 @@
 import time
-#import schedule
+import schedule
 import sqlite3
 import sys
-#import Adafruit_DHT
+
 
 
 dbname='PH_DB.db'
@@ -10,38 +10,37 @@ con = sqlite3.connect('PH_DB.db')
 
 with con:
     cur = con.cursor()
-    cur.execute("DROP TABLE IF NOT EXSITS PH_VALUE")
-    cur.execute("CREATE TABLE PH_SENSOR (TIMESTAMP DATETIME, VALUE NUMERIC, WATERTEMP NUMERIC)")
+    cur.execute("CREATE TABLE IF NOT EXISTS PH_SENSOR (TIMESTAMP DATETIME, VALUE NUMERIC, WATERTEMP NUMERIC)")
     print("DATABASE CREATED")
 
 
 
 #get data from PH sensor
-def getPHdata():
+#def getPHdata():
 	# Inset pin til vores ph sensor
     # Hvis der er flere pin objekter skal de indsættes her
-    print("")
+    #print("")
     # VALUE,WATERTEMP = VORES PIN SOM ER FORBUNDET TIL SENSOR SOM MÅLER PH VÆRDI
-    if "VALUE" is not None and "WATERTEMP" is not None:
-        VALUE = round(VALUE)
-        WATERTEMP = round(WATERTEMP,1)
+    #if "VALUE" is not None and "WATERTEMP" is not None:
+        #VALUE = round(VALUE)
+        #WATERTEMP = round(WATERTEMP,1)
     #return VALUE, WATERTEMP
 	
 
 # Set data into PH sensor db:
-def SetPHdata(VALUE, WATERTEMP):
-	print("Getting PH data:")
-	curs=sqlite3.connect('PH_DB.db')
-	curs.execute("INSERT INTO PH_VALUE values(datetime('now'), (?), (?)", (VALUE, WATERTEMP))
-	curs.commit()
-	curs.close()
+#def SetPHdata(VALUE, WATERTEMP):
+	#print("Getting PH data:")
+	#curs=sqlite3.connect('PH_DB.db')
+	#curs.execute("INSERT INTO PH_VALUE values(datetime('now'), (?), (?)", (VALUE, WATERTEMP))
+	#curs.commit()
+	#curs.close()
 
 # main function
-def main():
-	while True:
+#def main():
+	#while True:
 		#VALUE, WATERTEMP = getPHdata()
 		#SetPHdata (VALUE, WATERTEMO)
-		print("test")
+		#print("test")
 
 # ------------ Execute program 
-main()
+#main()
