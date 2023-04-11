abe34b87fd0796b867d04f1188072660ace20f14
chat0933 <90390114+chat0933@users.noreply.github.com>
2023-04-11 19:15:29 +0200

    testing
    test1

--- iot_projekt/app.py
+++ iot_projekt/app.py
@@ -1,59 +1,63 @@
 from datetime import datetime
-
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
 import io
 
-from flask import Flask, render_template, make_response, request
+from flask import Flask, render_template, send_file, make_response, request
 app = Flask(__name__)
 
 import sqlite3
-conn=sqlite3.connect('PH_DB.db', check_same_thread=False)
+conn=sqlite3.connect('PH_DB1.db', check_same_thread=False)
 curs=conn.cursor()
 
-# Retrieve LAST data from database
 def getLastData():
 	for row in curs.execute("SELECT * FROM PH_SENSOR ORDER BY timestamp DESC LIMIT 1"):
 		time = str(row[0])
-		PHVALUE = row[1]
-		GRADER = row[2]
+		ANALOG_VALUE = row[1]
+		PH_VALUE = row[2]
 	#conn.close()
-	return time, PHVALUE, GRADER
+	return time, ANALOG_VALUE, PH_VALUE
 
 # Get 'x' samples of historical data
 def getHistData (numSamples):
 	curs.execute("SELECT * FROM PH_SENSOR ORDER BY timestamp DESC LIMIT "+str(numSamples))
 	data = curs.fetchall()
 	dates = []
-	PHVALUES = []
-	GRADERS = []
+	ANALOG_VALUES = []
+	PH_VALUES = []
 	for row in reversed(data):
 		dates.append(row[0])
-		PHVALUES.append(row[1])
-		GRADERS.append(row[2])
-		PHVALUES, GRADERS = Data(PHVALUES, GRADERS)
-	return dates, PHVALUES, GRADERS
-
-# Test data for cleanning possible "out of range" values
-def Data(PHVALUES, GRADERS):
-	n = len(PHVALUES)
-	for i in range(0, n-1):
-		if (PHVALUES[i] < -10 or PHVALUES[i] >50):
-			PHVALUES[i] = PHVALUES[i-2]
-		if (GRADERS[i] < 0 or GRADERS[i] >100):
-			GRADERS[i] = PHVALUES[i-2]
-	return PHVALUES, GRADERS
-
+		ANALOG_VALUES.append(row[1])
+		PH_VALUES.append(row[2])
+	return dates, ANALOG_VALUES, PH_VALUES
 
 # Get Max number of rows (table size)
 def maxRowsTable():
-	for row in curs.execute("SELECT COUNT(PHVALUE) from  PH_SENSOR"):  #COUNT = COLUMN NAME
+	for row in curs.execute("SELECT COUNT(ANALOG_VALUE) from  PH_SENSOR"):  #COUNT = COLUMN NAME
 		maxNumberRows=row[0]
 	return maxNumberRows
 
+# define and initialize global variables
+global numSamples
+numSamples = maxRowsTable()
+if (numSamples > 101):
+        numSamples = 100
+
+
+# Test data for cleanning possible "out of range" values
+def Data(ANALOG_VALUES, PH_VALUES):
+	n = len(ANALOG_VALUES)
+	for i in range(0, n-1):
+		if (ANALOG_VALUES[i] < -10 or ANALOG_VALUES[i] >50):
+			ANALOG_VALUES[i] = ANALOG_VALUES[i-2]
+		if (PH_VALUES[i] < 0 or PH_VALUES[i] >100):
+			PH_VALUES[i] = ANALOG_VALUES[i-2]
+	return ANALOG_VALUES, PH_VALUES
+
+
 # Get sample frequency in minutes
 def freqSample():
-	times, PHVALUES, GRADERS = getHistData (2)
+	times, ANALOG_VALUES, PH_VALUES = getHistData (2)
 	fmt = '%Y-%m-%d %H:%M:%S'
 	tstamp0 = datetime.strptime(times[0], fmt)
 	tstamp1 = datetime.strptime(times[1], fmt)
@@ -61,11 +65,6 @@ def freqSample():
 	freq = int(round(freq.total_seconds()/60))
 	return (freq)
 
-# define and initialize global variables
-global numSamples
-numSamples = maxRowsTable()
-if (numSamples > 101):
-        numSamples = 100
 
 global freqSamples
 freqSamples = freqSample()
@@ -77,11 +76,11 @@ rangeTime = 100
 # main route 
 @app.route("/")
 def index():
-	time, PHVALUE, GRADER = getLastData()
+	time, ANALOG_VALUE, PH_VALUE = getLastData()
 	templateData = {
 	  'time'		: time,
-      'PHVALUE'		: PHVALUE,
-      'GRADER'		: GRADER,
+      'ANALOG_VALUE'		: ANALOG_VALUE,
+      'PH_VALUE'		:PH_VALUE,
       'freq'		: freqSamples,
       'rangeTime'		: rangeTime
 	}
@@ -101,25 +100,24 @@ def my_form_post():
     if (numSamples > numMaxSamples):
         numSamples = (numMaxSamples-1)
     
-    time, PHVALUE, GRADER = getLastData()
+    time, ANALOG_VALUE, PH_VALUE = getLastData()
     
     templateData = {
 	  'time'		: time,
-      'PHVALUE'		: PHVALUE,
-      'GRADER'		: GRADER,
+      'ANALOG_VALUE'		: ANALOG_VALUE,
+      'PH_VALUE'		: PH_VALUE,
       'freq'		: freqSamples,
       'rangeTime'	: rangeTime
 	}
     return render_template('index.html', **templateData)
 	
 	
-@app.route('/plot/PHVALUE')
-def plot_PHVALUE():
-	times, PHVALUES, GRADERS = getHistData(numSamples)
-	ys = PHVALUES
+def plot_ANALOG_VALUES():
+	times, ANALOG_VALUES, PH_VALUES  = getHistData(numSamples)
+	ys = ANALOG_VALUES
 	fig = Figure()
 	axis = fig.add_subplot(1, 1, 1)
-	axis.set_title("PHVALUES")
+	axis.set_title("ANALOG_VALUES")
 	axis.set_xlabel("Samples")
 	axis.grid(True)
 	xs = range(numSamples)
@@ -131,23 +129,59 @@ def plot_PHVALUE():
 	response.mimetype = 'image/png'
 	return response
 
-#@app.route('/plot/GRADER')
-#def plot_hum():
-#	times, PHVALUES, GRADERS = getHistData(numSamples)
-#	ys = GRADERS
-#	fig = Figure()
-#	axis = fig.add_subplot(1, 1, 1)
-#	axis.set_title("GRADERS [0C]")
-#	axis.set_xlabel("Samples")
-#	axis.grid(True)
-#	xs = range(numSamples)
-#	axis.plot(xs, ys)
-#	canvas = FigureCanvas(fig)
-#	output = io.BytesIO()
-#	canvas.print_png(output)
-#	response = make_response(output.getvalue())
-#	response.mimetype = 'image/png'
-#	return response
+def plot_PH_VALUES():
+	times, ANALOG_VALUES, PH_VALUES  = getHistData(numSamples)
+	ys =  PH_VALUES
+	fig = Figure()
+	axis = fig.add_subplot(1, 1, 1)
+	axis.set_title("PH_VALUES")
+	axis.set_xlabel("Samples")
+	axis.grid(True)
+	xs = range(numSamples)
+	axis.plot(xs, ys)
+	canvas = FigureCanvas(fig)
+	output = io.BytesIO()
+	canvas.print_png(output)
+	response = make_response(output.getvalue())
+	response.mimetype = 'image/png'
+	return response
+
+@app.route('/plot/ANALOG_VALUES')
+def plot_ANALOG_VALUES():
+	times, ANALOG_VALUES, PH_VALUES  = getHistData(numSamples)
+	ys = ANALOG_VALUES
+	fig = Figure()
+	axis = fig.add_subplot(1, 1, 1)
+	axis.set_title("ANALOG_VALUES")
+	axis.set_xlabel("Samples")
+	axis.grid(True)
+	xs = range(numSamples)
+	axis.plot(xs, ys)
+	canvas = FigureCanvas(fig)
+	output = io.BytesIO()
+	canvas.print_png(output)
+	response = make_response(output.getvalue())
+	response.mimetype = 'image/png'
+	return response
+
+@app.route('/plot/PH_VALUES')
+def plot_PH_VALUES():
+	times, ANALOG_VALUES, PH_VALUES  = getHistData(numSamples)
+	ys = PH_VALUES
+	fig = Figure()
+	axis = fig.add_subplot(1, 1, 1)
+	axis.set_title("PH_VALUES")
+	axis.set_xlabel("Samples")
+	axis.grid(True)
+	xs = range(numSamples)
+	axis.plot(xs, ys)
+	canvas = FigureCanvas(fig)
+	output = io.BytesIO()
+	canvas.print_png(output)
+	response = make_response(output.getvalue())
+	response.mimetype = 'image/png'
+	return response
+
 	
 if __name__ == "__main__":
-   app.run(host='127.0.0.1', port=80, debug=False)
+   app.run(host='0.0.0.0', port=80, debug=False)
